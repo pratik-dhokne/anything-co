fb5575698288da5e89017f5661dfc7e73481af92
Pratik Dhokne <pratikdhokne1234@gmail.com>
2026-08-05 17:46:28 +0530

    some changes2

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,15 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals.js";
-import nextTs from "eslint-config-next/typescript.js";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
 
-export default defineConfig(
-  nextVitals,
-  nextTs,
-  globalIgnores([
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ])
-);
+export default [
+  ...nextVitals,
+  ...nextTs,
+  {
+    ignores: [
+      ".next/**",
+      "out/**",
+      "build/**",
+      "next-env.d.ts",
+    ],
+  },
+];
